893fb6fd2a5a9255fbbdbb920233a3fcbc33da8a
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-09-01 13:37:20 -0600

    implement board using hashmap and reset board method

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,9 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,19 +11,21 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private HashMap<ChessPosition, ChessPiece> board;
 
     public ChessBoard() {
-        
+        this.board = new HashMap<ChessPosition, ChessPiece>();
     }
 
     /**
      * Adds a chess piece to the chessboard
+     * Replaces old piece
      *
      * @param position where to add the piece to
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        this.board.put(position, piece);
     }
 
     /**
@@ -30,7 +36,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return this.board.get(position);
     }
 
     /**
@@ -38,6 +44,50 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        addPieces(ChessGame.TeamColor.WHITE);
+        addPieces(ChessGame.TeamColor.BLACK);
+    }
+
+    private void addPieces(ChessGame.TeamColor color) {
+        ChessPiece.PieceType type = ChessPiece.PieceType.ROOK;
+        int row = 1;
+        int pawnRow = 2;
+        if(color == ChessGame.TeamColor.BLACK) {
+            row = 8;
+            pawnRow = 7;
+        }
+
+        addPiece(new ChessPosition(row, 1), new ChessPiece(color, type));
+        addPiece(new ChessPosition(row, 8), new ChessPiece(color, type));
+
+        type = ChessPiece.PieceType.KNIGHT;
+        addPiece(new ChessPosition(row, 2), new ChessPiece(color, type));
+        addPiece(new ChessPosition(row, 7), new ChessPiece(color, type));
+
+        type = ChessPiece.PieceType.BISHOP;
+        addPiece(new ChessPosition(row, 3), new ChessPiece(color, type));
+        addPiece(new ChessPosition(row, 6), new ChessPiece(color, type));
+
+        type = ChessPiece.PieceType.PAWN;
+        for(int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(pawnRow, i), new ChessPiece(color, type));
+        }
+
+        addPiece(new ChessPosition(row, 4), new ChessPiece(color, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(row, 5), new ChessPiece(color, ChessPiece.PieceType.KING));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.equals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(board);
     }
 }
