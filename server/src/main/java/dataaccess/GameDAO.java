530cf50230a0d853858bf544e375665045457875
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-10-12 19:56:16 -0600

    write the gameDAO interface

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import model.GameData;
+
 public interface GameDAO {
+    public void insertGame(GameData game) throws DataAccessException;
+    public void updateGame(int gameID, GameData game) throws DataAccessException;
+    public GameData selectGame(int gameID) throws DataAccessException;
+    public GameData[] selectAllGames() throws DataAccessException;
+    public void clearGames() throws DataAccessException;
 }
