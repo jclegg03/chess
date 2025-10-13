66a74d74f71982c2b908959049641e16694aec86
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-10-12 19:48:30 -0600

    write the AuthDAO interface

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    public void insertAuth(AuthData auth) throws DataAccessException;
+    public void selectAuth(String authToken) throws DataAccessException;
+    public void clearAuths() throws  DataAccessException;
 }
