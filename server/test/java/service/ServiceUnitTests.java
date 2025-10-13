e8667ce16034d953a2648834f476edc703ae5c7a
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-10-12 22:02:43 -0600

    Make and run getUser test

--- server/test/java/service/ServiceUnitTests.java
+++ server/test/java/service/ServiceUnitTests.java
@@ -6,22 +6,26 @@ import model.UserData;
 
 import java.util.Objects;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ServiceUnitTests {
 
     private final static Service service = new Service();
+    UserData sampleUser = new UserData("username", "password", "email@mail.com");
+    AuthData sampleAuth = new AuthData("username", "" + Objects.hash("username", "password"));
 
     @Test
     public void createUserSuccess() {
-        UserData sampleUser = new UserData("username", "password", "email@mail.com");
-        AuthData sampleAuth = new AuthData("username", "" + Objects.hash("username", "password"));
         AuthData auth = Service.createUser(sampleUser);
-
         assert auth.equals(sampleAuth);
     }
 
     @Test
 //      Unfortunately it is impossible to test getUser without first implementing addUser.
     public void getUserSuccess() {
-        UserData user = service.getUser("username");
+        assert Service.getUser("username") == null;
+
+        Service.createUser(sampleUser);
+        assert Service.getUser(sampleUser.username()).equals(sampleUser);
     }
 }
