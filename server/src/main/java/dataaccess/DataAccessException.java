f9a728ae1c2a5380be33baa5b369ef617c4e9b10
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-10-16 22:40:46 -0600

    Change DataAccessException to have a private status

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -6,7 +6,11 @@ import io.javalin.http.HttpStatus;
  * Indicates there was an error connecting to the database
  */
 public class DataAccessException extends Exception{
-    public final HttpStatus status;
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    private final HttpStatus status;
     public DataAccessException(String message, HttpStatus status) {
         super(message);
         this.status = status;
@@ -15,5 +19,4 @@ public class DataAccessException extends Exception{
         super(message, ex);
         this.status = status;
     }
-
 }
