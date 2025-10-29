b234c3197ae01f10cee614116ce0bd3580f5c6e7
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-10-29 17:45:42 -0600

    Set up DatabaseAuthDAO

--- server/src/main/java/dataaccess/auth/DatabaseAuthDAO.java
+++ server/src/main/java/dataaccess/auth/DatabaseAuthDAO.java
@@ -1,26 +1,64 @@
 package dataaccess.auth;
 
 import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import dataaccess.user.DatabaseUserDAO;
 import model.AuthData;
+import model.UserData;
 
 public class DatabaseAuthDAO implements AuthDAO {
+    public DatabaseAuthDAO() throws DataAccessException {
+        String init = "CREATE TABLE IF NOT EXISTS auth(\n" +
+                "id INT NOT NULL AUTO_INCREMENT,\n" +
+                "username VARCHAR(100) NOT NULL UNIQUE,\n" +
+                "token CHAR(36) NOT NULL UNIQUE,\n" +
+                "PRIMARY KEY (id),\n" +
+                "INDEX(token)\n" +
+                ")ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;";
+
+        DatabaseManager.executeVoidStatement(init);
+    }
+
     @Override
     public void insertAuth(AuthData auth) throws DataAccessException {
+        String statement = "INSERT INTO auth (username, token)\n";
+        String values = "VALUES ('";
 
+        values += auth.username() + "', '";
+        values += auth.authToken() + "');";
+
+        statement += values;
+
+        DatabaseManager.executeVoidStatement(statement);
     }
 
     @Override
     public AuthData selectAuth(String authToken) throws DataAccessException {
-        return null;
+        String statement = "SELECT username FROM auth WHERE token = '" +
+                authToken + "';";
+        var res = DatabaseManager.executeSelectStatement(statement,
+                result -> new AuthData(
+                        result.getString("username"),
+                        authToken
+                ));
+
+        if(res.isEmpty()) {
+            return null;
+        }
+
+        return res.getFirst();
     }
 
     @Override
     public void deleteAuth(AuthData auth) throws DataAccessException {
+        String statement = "DELETE FROM auth WHERE token = '"
+                + auth.authToken() + "';";
 
+        DatabaseManager.executeVoidStatement(statement);
     }
 
     @Override
     public void clearAuths() throws DataAccessException {
-
+        DatabaseManager.executeVoidStatement("TRUNCATE auth;");
     }
 }
--- server/src/test/java/dataaccess/AuthDAOTest.java
+++ server/src/test/java/dataaccess/AuthDAOTest.java
@@ -1,34 +1,105 @@
 package dataaccess;
 
+import dataaccess.auth.DatabaseAuthDAO;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
 class AuthDAOTest {
+    DatabaseAuthDAO authDAO;
+    String authToken = UUID.randomUUID().toString();
+    AuthData auth = new AuthData("username", authToken);
+
+    {
+        try {
+            authDAO = new DatabaseAuthDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        try {
+            authDAO.clearAuths();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void insertAuthSuccess() {
+        try {
+            authDAO.insertAuth(auth);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void insertAuthFail() {
+        try {
+            authDAO.insertAuth(auth);
+            assertThrows(DataAccessException.class, () -> authDAO.insertAuth(auth));
+            assertThrows(DataAccessException.class, () -> authDAO.insertAuth(new AuthData("username", "")));
+            assertThrows(DataAccessException.class, () -> authDAO.insertAuth(new AuthData("", authToken)));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void selectAuthSuccess() {
+        try {
+            authDAO.insertAuth(auth);
+            assertEquals(auth, authDAO.selectAuth(authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void selectAuthFail() {
+        try {
+            assertNull(authDAO.selectAuth(authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void deleteAuthSuccess() {
+        try {
+            authDAO.insertAuth(auth);
+            authDAO.deleteAuth(auth);
+            assertNull(authDAO.selectAuth(authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void deleteAuthFail() {
+        try {
+            authDAO.deleteAuth(auth);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void clearAuths() {
+        try {
+            authDAO.clearAuths();
+            authDAO.insertAuth(auth);
+            authDAO.clearAuths();
+            assertNull(authDAO.selectAuth(authToken));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
