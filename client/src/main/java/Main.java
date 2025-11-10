c6817fb8adc26c85c71ae034c6ddfa31a26aeda6
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-11-10 07:38:36 -0700

    Made a basic user class

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -3,6 +3,7 @@ import ui.BoardPrinter;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to chess! Type help for a list of commands, or quit to exit.");
         var board = new ChessBoard();
         board.resetBoard();
         BoardPrinter.print(board, ChessGame.TeamColor.BLACK);
---
+++ client/src/main/java/User.java
@@ -0,0 +1,22 @@
+public class User {
+    private boolean isLoggedIn;
+    private String authToken;
+
+    public User() {
+        isLoggedIn = false;
+        authToken = null;
+    }
+
+    public void login(String authToken) {
+        isLoggedIn = true;
+        this.authToken = authToken;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+}
