13997aa3e7996e2920aaa7a2abda48e402c61920
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-11-11 21:03:40 -0700

    Implement logged in print help function

--- client/src/main/java/ui/CLIManager.java
+++ client/src/main/java/ui/CLIManager.java
@@ -14,6 +14,7 @@ public class CLIManager {
     private final String[] expectedLogin = {"login", "<username>", "<password>"};
     private final String[] expectedRegister = {"register", "<username>", "<password>", "<email>"};
     private final ServerFacade serverFacade;
+    private final String expectedPlay = "play <game id number> <player piece color>";
 
     public CLIManager(User user, ServerFacade serverFacade) {
         this.user = user;
@@ -74,7 +75,27 @@ public class CLIManager {
     }
 
     private void printHelpLoggedIn() {
-        //TODO
+        printDefaultHelp();
+        String[] helpStrings = {
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "logout" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - logs you out",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "create <game name>" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - creates a chess game of the specified name",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "list" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - lists all games currently available on the server",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + expectedPlay + EscapeSequences.RESET_TEXT_COLOR
+                        + " - play the specified game with the specified piece color",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + expectedPlay + EscapeSequences.RESET_TEXT_COLOR
+                        + " - watch the specified game"
+        };
+
+        for(var string : helpStrings) {
+            System.out.println(string);
+        }
     }
 
     private void createGame(String[] inputs) {
@@ -91,10 +112,9 @@ public class CLIManager {
     }
 
     private void joinGame(String[] inputs) {
-        var expected = "join <game id number> <player piece color>";
         try {
-            var gameID = validateGameIDInput(inputs, expected);
-            var team = validateTeamColor(inputs, expected);
+            var gameID = validateGameIDInput(inputs, expectedPlay);
+            var team = validateTeamColor(inputs, expectedPlay);
             serverFacade.joinGame(user.getAuthToken(), gameID, team);
         } catch (RuntimeException _) {
         }
@@ -110,18 +130,16 @@ public class CLIManager {
     }
 
     private ChessGame.TeamColor validateTeamColor(String[] inputs, String expected) {
-        if(inputs.length < 3) {
+        if (inputs.length < 3) {
             invalidInput(inputs, expected);
         }
 
         var color = inputs[2];
-        if(color.equalsIgnoreCase("white")) {
+        if (color.equalsIgnoreCase("white")) {
             return ChessGame.TeamColor.WHITE;
-        }
-        else if(color.equalsIgnoreCase("black")) {
+        } else if (color.equalsIgnoreCase("black")) {
             return ChessGame.TeamColor.BLACK;
-        }
-        else {
+        } else {
             invalidInput(inputs, expected);
             System.out.println("Player piece color must be either \"black\" or \"white\". Case doesn't matter.");
             throw new RuntimeException();
@@ -129,7 +147,7 @@ public class CLIManager {
     }
 
     private int validateGameIDInput(String[] inputs, String expected) {
-        if(inputs.length < 2) {
+        if (inputs.length < 2) {
             invalidInput(inputs, expected);
         }
         try {
@@ -168,12 +186,7 @@ public class CLIManager {
     }
 
     private void printHelpLoggedOut() {
-        var helpString = EscapeSequences.SET_TEXT_COLOR_GREEN + "help" + EscapeSequences.RESET_TEXT_COLOR +
-                " - Tells you what commands you can use";
-        var quitString = EscapeSequences.SET_TEXT_COLOR_GREEN + "quit" + EscapeSequences.RESET_TEXT_COLOR +
-                " - Exits the program";
-        System.out.println(helpString);
-        System.out.println(quitString);
+        printDefaultHelp();
         var loginString = new StringBuilder(EscapeSequences.SET_TEXT_COLOR_GREEN);
         for (String item : expectedLogin) {
             loginString.append(item);
@@ -190,6 +203,15 @@ public class CLIManager {
         registerString.append(EscapeSequences.RESET_TEXT_COLOR);
         registerString.append("- to create a user and login to our great server");
         System.out.println(registerString);
+    }
+
+    private void printDefaultHelp() {
+        var helpString = EscapeSequences.SET_TEXT_COLOR_GREEN + "help" + EscapeSequences.RESET_TEXT_COLOR +
+                " - Tells you what commands you can use";
+        var quitString = EscapeSequences.SET_TEXT_COLOR_GREEN + "quit" + EscapeSequences.RESET_TEXT_COLOR +
+                " - Exits the program";
+        System.out.println(helpString);
+        System.out.println(quitString);
     }
 
     private void invalidInput(String[] inputs) {
