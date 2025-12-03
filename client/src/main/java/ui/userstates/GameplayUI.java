f1d4ea8e12d9c2199ad04f3a084a89fb838b6873
Jay Clegg <89157780+jclegg03@users.noreply.github.com>
2025-12-03 09:51:20 -0700

    write gameplay help text

--- client/src/main/java/ui/userstates/GameplayUI.java
+++ client/src/main/java/ui/userstates/GameplayUI.java
@@ -2,6 +2,7 @@ package ui.userstates;
 
 import model.User;
 import server.ServerFacade;
+import ui.EscapeSequences;
 
 public class GameplayUI extends CLIUserInterface {
 
@@ -18,7 +19,7 @@ public class GameplayUI extends CLIUserInterface {
             case "help" -> help();
             case "redraw" -> redraw();
             case "leave" -> leave();
-            case "make" -> makeMove();
+            case "make", "move" -> makeMove();
             case "resign" -> resign();
             case "highlight" -> highlightLegalMoves();
             default -> defaultInput();
@@ -29,6 +30,31 @@ public class GameplayUI extends CLIUserInterface {
 
     protected void help() {
         super.help();
+
+        String[] helpStrings = {
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "redraw chess board" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - prints the chess board again to fix any weird looking boards. Make sure to increase window" +
+                        " size if errors persist",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "leave" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - exits the game without resigning",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "make move <start position> <end position>" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - moves the piece on the start position to the end position, or yells at you " +
+                        "if you try something illegal. You will be prompted select a promotion type if a move leads" +
+                        " to a pawn promotion",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "resign" + EscapeSequences.RESET_TEXT_COLOR
+                        + " - prompts you to confirm that you want to resign, then ends the game. Use leave to quit " +
+                        "without ending the game",
+
+                EscapeSequences.SET_TEXT_COLOR_GREEN + "highlight legal moves <position>" +
+                        EscapeSequences.RESET_TEXT_COLOR + " - shows all moves the piece at the specified position can make"
+        };
+
+        for (var string : helpStrings) {
+            System.out.println(string);
+        }
     }
 
     private void redraw() {
